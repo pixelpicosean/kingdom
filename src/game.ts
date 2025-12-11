0f8c6f34d53f7190f447ef31f5bab30633397f33
Jessie <jessie.d.snow@gmail.com>
2025-12-11 19:18:49 +0800

    draw face

--- src/game.ts
+++ src/game.ts
@@ -12,3 +12,17 @@ ctx.font = '30px Arial';
 ctx.textBaseline = 'top';
 ctx.textAlign = 'left';
 ctx.fillText('Hello, world!', 0, 0);
+
+ctx.beginPath();
+ctx.ellipse(canvas.width/2, canvas.height/2, 80, 80, 0, 0, Math.PI * 2);
+ctx.fill();
+
+ctx.fillStyle = 'black';
+
+ctx.beginPath();
+ctx.ellipse(canvas.width/2-25, canvas.height/2-40, 15, 15, 0, 0, Math.PI * 2, true);
+ctx.fill();
+
+ctx.beginPath();
+ctx.ellipse(canvas.width/2+55, canvas.height/2+30, 15, 15, 0, 0, Math.PI * 2, true);
+ctx.fill();
